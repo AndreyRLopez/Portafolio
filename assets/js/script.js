fbe94f456dd0756b7b3db6a21deb9fbbd4e378c8
AndreyRLopez <andreylopeziarl@gmail.com>
2023-06-11 10:45:35 -0500

    Agregamos el proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    
+    $(window).scroll(function () {
+    // Scroll navbar start
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+    // Scroll navbar end
+  
+    // Scroll Button start
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
+    // Scroll Button end
+    });
+  
+    // Scroll up Button start
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+    });
+    // Scroll up Button end
+  
+    // Typing start
+    var typed = new Typed(".typing", {
+        strings: ["estudiante de Ingeniería de Software", "Front End Developer", "C++ Developer", "PHP Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+    var typed = new Typed(".typing1", {
+        strings: ["estudiante de Ingeniería de Software", "Front End Developer", "C++ Developer", "PHP Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+    // Typing end
+
+  
+    // Toggle start
+    $(".menu-btn").click(function () {
+      $(".navbar .menu").toggleClass("active");
+      $(".menu-btn i").toggleClass("active");
+    });
+    // Toggle end
+});
